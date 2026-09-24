09d0f1be9508506d4704a1adda577cd75a6e7482
Murilo AlvesII <anime.dragon.sheques@gmail.com>
2026-09-24 16:39:20 -0300

    feat: adiciona segundo arquivo com explicação sobre comentários em Python

---
+++ Comentarios.py
@@ -0,0 +1,21 @@
+"""
+COMENTÁRIOS (COMMENTARIUM)
+
+    Iniciaremos vendo o que é um comentário em Python e como ele pode ser utilizado no código.
+
+    Primeiramente, vale ressaltar que, para criar um comentário em Python, 
+    utilizamos o símbolo #, chamado de cerquilha.
+
+    Tudo o que for escrito depois desse símbolo, na mesma linha, 
+    será ignorado pelo interpretador do Python. Dessa forma, podemos escrever observações e 
+    explicações no código sem que elas sejam executadas.
+
+    Vale ressaltar que esta informação que você está lendo não é um comentário, mas sim uma 
+    docstring, algo que veremos futuramente.
+"""
+
+# Isto é um comentário
+
+print("Hello World, I AM DRAGON!!") # Comentário após uma instrução
+
+# Este comentário também será ignorado pelo interpretador.
