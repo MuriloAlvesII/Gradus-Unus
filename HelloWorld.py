2b5330d62cd30e429dca67917867460da498b6a1
Murilo AlvesII <anime.dragon.sheques@gmail.com>
2026-09-24 13:46:37 -0300

    feat: implementacao de HelloWorld

---
+++ HelloWorld.py
@@ -0,0 +1,7 @@
+# Vamos inicia usando uma função nativa do python, chamada de print().
+# Onde e resposavel por aprenta dados para o usuario por meio do terminal.
+
+# Para mante a tradição, feita ao se aprende sobre qualque liguagem copuntacional, 
+# iniciaremos com um print("Hello World!").
+
+print("Hello World!")
--- README.md
+++ README.md
@@ -6,4 +6,6 @@
 
 # Matérias de apoio
 
+- [Python.Land](https://python.land/python-tutorial)
+
 > Update V0.1
