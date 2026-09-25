0d7db54d36938dbfbacdd77584165c75afe3e21a
Murilo AlvesII <anime.dragon.sheques@gmail.com>
2026-09-25 09:57:08 -0300

    fix: correção de ortografia no codigo.

--- Variaveis.py
+++ Variaveis.py
@@ -21,9 +21,9 @@ idade = 2000
 cidade = "Pau dos Ferros"  
 
 # Podemos utilizar o nome da variável para acessar o objeto associado a ele.
-print("Ex.1:\n Ola!\n Meu nome é" + "" + nome)
-print("Idade:", idade) 
-print("Cidade:", cidade)
+print("Ex.1:\n Ola!\n Meu nome é ", nome)
+print(" Idade:", idade) 
+print(" Cidade:", cidade)
 
 # Ex. 2: 
 # Podemos utilizar variáveis para realizar operações.
@@ -32,36 +32,36 @@ ano_nascimento = 26
 
 idade = ano_atual - ano_nascimento
 
-print("Ex.2:\n Eu nasci no ano ", ano_nascimento)
-print("Minha idade é", idade)
+print("Ex.2:\n Eu nasci no ano ", ano_nascimento,".")
+print(" Minha idade é", idade,".")
 
 # Ex. 3:
 # Uma variável pode ser associada a outro objeto posteriormente.
 ano = 2026
 
-print("Ex.3:\n Estamos no ano de ", ano, ".")
+print("Ex.3:\n Estamos no ano de", ano,".")
 
 ano = 2030
 
-print("Ex.3:\n Estamos no ANO DE ", ano, "!!!!????")
+print(" Estamos no ANO DE", ano,"!!!!????")
 # Depois da nova atribuição, o nome "ano" passa a estar associado ao objeto 2030.
 
 # Ex. 4:
 # Podemos realizar múltiplas atribuições em uma única instrução.
 nome, idade, cidade = "Frank", 15, "Água Nova"
 
-print("Ex.4:\n Meu nome é ", nome, "!")
-print("Moro na cidade de", cidade, ".") 
-print("Tenho ", idade, " anos de idade.")
+print("Ex.4:\n Meu nome é", nome,"!")
+print(" Moro na cidade de", cidade,".") 
+print(" Tenho", idade, "anos de idade.")
 
 # Ex. 5:
 # Também podemos associar o mesmo objeto a vários nomes.
 a = b = c = d = 0
 
 print("Ex.5:\n a = ", a)
-print("b =", b)
-print("c =", c) 
-print("d =", d) # Nesse caso, os quatro nomes foram associados ao objeto 0.
+print(" b =", b)
+print(" c =", c) 
+print(" d =", d) # Nesse caso, os quatro nomes foram associados ao objeto 0.
 
 # Nomes válidos:
 
